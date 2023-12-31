8cfeb29e384e290f5bb97db3a1bfdb225d50c8fc
dyoshioka-555 <yoshioka.daiki@g.sp.m.is.nagoya-u.ac.jp>
2023-12-31 17:32:54 +0900

    change list

--- make_set_multi.py
+++ make_set_multi.py
@@ -13,19 +13,26 @@ set[1] = {"a": [], "b": []}
 set1_a, set1_b, set2_a, set2_b = [], [], [], []
 
 # 各テキストに対して異なる順序でAB、AC、BCのペアを作成
-for text in texts:
-    pairs = [("A", "B"), ("A", "C"), ("B", "C")]
-    shuffle(pairs)  # ペアの順序をシャッフル
+for text, i in zip(texts, range(len(texts))):
+    pairs = [["A", "B"], ["A", "C"], ["B", "C"]]
+    shuffle(pairs)
 
     # シャッフルされたペアをセット1に追加
     for pair in pairs:
-        set[0]["a"].append(f"wav/set1/{pair[0]}/csj_{text}.wav")
-        set[0]["b"].append(f"wav/set1/{pair[1]}/csj_{text}.wav")
+        shuffle(pair)
+        if i % 2:
+            set[0]["a"].append(f"wav/set1/{pair[0]}/csj_{text}.wav")
+            set[0]["b"].append(f"wav/set1/{pair[1]}/csj_{text}.wav")
 
-    shuffle(pairs)  # 再度シャッフルしてセット2に異なる順序で追加
-    for pair in pairs:
-        set[1]["a"].append(f"wav/set2/{pair[1]}/csj_{text}.wav")  # 順番を逆にして追加
-        set[1]["b"].append(f"wav/set2/{pair[0]}/csj_{text}.wav")
+            set[1]["a"].append(f"wav/set2/{pair[1]}/csj_{text}.wav")  # 順番を逆にして追加
+            set[1]["b"].append(f"wav/set2/{pair[0]}/csj_{text}.wav")
+        else:
+            set[0]["a"].append(f"wav/set1/{pair[1]}/csj_{text}.wav")
+            set[0]["b"].append(f"wav/set1/{pair[0]}/csj_{text}.wav")
+
+            set[1]["a"].append(f"wav/set2/{pair[0]}/csj_{text}.wav")  # 順番を逆にして追加
+            set[1]["b"].append(f"wav/set2/{pair[1]}/csj_{text}.wav")
+
 
 for i in [1, 2]:
     for x in ["a", "b"]:
